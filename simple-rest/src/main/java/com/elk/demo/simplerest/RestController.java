d6b72a0d3fe2c7afb0e8468fc61fc7c105a0f156
Manujay <myadav@marketaxess.com>
2021-05-06 13:39:24 +0530

    Initial Commit

---
+++ simple-rest/src/main/java/com/elk/demo/simplerest/RestController.java
@@ -0,0 +1,17 @@
+package com.elk.demo.simplerest;
+
+import java.time.Instant;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RestController {
+
+	@GetMapping("/check")
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok(String.format("Service is up, called at system time %s", Instant.now()));
+	}
+
+}
